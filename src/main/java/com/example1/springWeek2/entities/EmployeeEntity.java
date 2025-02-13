7677f007dc632502ef6896cb925d5c864d6647a6
Ayush-Ydav <anayayushyadav275205@gmail.com>
2025-02-13 18:25:23 +0530

    J Unit testing

---
+++ src/main/java/com/example1/springWeek2/entities/EmployeeEntity.java
@@ -0,0 +1,30 @@
+package com.example1.springWeek2.entities;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String name;
+
+    private Long salary;
+
+}
